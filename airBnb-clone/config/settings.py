7deff4234807eb0c4192b34fc0ce29fd95bf1b48
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-01-25 17:53:49 +0900

    list, reservation, review

--- airBnb-clone/config/settings.py
+++ airBnb-clone/config/settings.py
@@ -45,6 +45,9 @@ PROJECT_APPS = [
     "core.apps.CoreConfig",
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
 ]
 
 
--- airBnb-clone/reviews/models.py
+++ airBnb-clone/reviews/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from core import models as core_models
+from users import models as users_models
+from rooms import models as rooms_models
 
-# Create your models here.
+
+class Review (core_models.TimeStampedModel):
+
+    """ Review Modle Definition """
+
+    content = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+
+    user = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
+    room = models.ForeignKey(rooms_models.Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.content} - {self.room}'
